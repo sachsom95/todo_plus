c8674d3d0238be9756bbb74000475687162bed32
Suhani Chawla <suhanichawla2000@gmail.com>
2021-02-12 16:43:34 +0530

    set up todo icon, side panel and basic todolist

---
+++ extension/webviews/components/sidebar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    let todos:Array<{text: string;completed:boolean}>=[];
+    let text="";
+</script>
+<style>
+    h2{
+        color:orange;
+    }
+    .completed{
+        text-decoration: line-through;
+    }
+</style>
+<h2>Todolist: Make todos here!</h2>
+<form on:submit|preventDefault={()=>{
+    todos=[...todos,{text,completed:false}];
+    text="";
+}}>
+    <input bind:value={text}  />
+</form>
+<ul>
+    {#each todos as todo (todo.text)}
+        <li class:completed={todo.completed}
+            on:click={()=>{
+                todo.completed=!todo.completed
+            }}
+        >{todo.text}</li>
+    {/each}
+</ul>
