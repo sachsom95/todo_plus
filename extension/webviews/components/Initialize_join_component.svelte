b017392cf8a88052ee23b09bccf66b36f09edf09
sachsom95 <sachin.soman@ucdconnect.ie>
2021-02-17 07:15:16 +0000

    Add Initializion and join UI component in svelte

---
+++ extension/webviews/components/Initialize_join_component.svelte
@@ -0,0 +1,29 @@
+<p class="p_heading">Create new collaborative todo list<br /></p>
+<p class="p_content">
+    Use this option if you want to start a new todo list to share with other
+    people. A collaboration code will be generated
+</p>
+
+<button>Initialize todo list</button>
+<p class="p_heading">Connect to existing Todo list<br /></p>
+<p class="p_content">
+    Use this option if you want to join an existing todo list with an invite
+    code
+</p>
+<button>Join a todo list</button>
+
+<style>
+    .p_heading {
+        font-size: var(--vscode-font-size);
+        font-weight: bold;
+        font-family: var(--vscode-font-family);
+    }
+
+    .p_content {
+        font-weight: 400;
+    }
+
+    ::placeholder {
+        text-align: center;
+    }
+</style>
