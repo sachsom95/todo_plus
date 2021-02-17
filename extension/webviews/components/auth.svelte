80f5bbb6a0b27ad68b9c4f20ce9bc22785c280e1
sachsom95 <sachin.soman@ucdconnect.ie>
2021-02-17 22:40:45 +0000

    Convert Auth using github and Anon to a component

---
+++ extension/webviews/components/auth.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+</script>
+
+<div class="parent">
+    <br />
+    <button type="submit">
+        <div class="icon">
+            <i class="codicon codicon-github-inverted icon-align-fix" />
+            Login with Github
+        </div>
+    </button>
+    <br />
+    <button type="submit">
+        <div class="icon">
+            <i class="codicon codicon-account icon-align-fix" />
+            Sign In Anonymously
+        </div>
+    </button>
+</div>
+
+<style>
+    .parent {
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+    .icon-align-fix {
+        position: relative;
+        margin-right: 2px;
+        top: 2px;
+    }
+</style>
