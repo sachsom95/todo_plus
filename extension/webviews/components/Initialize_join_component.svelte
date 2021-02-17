988bf453975e4e47e7184fb84bb571db86c26df0
sachsom95 <sachin.soman@ucdconnect.ie>
2021-02-17 22:39:51 +0000

    Add Event listner to update state change

--- extension/webviews/components/Initialize_join_component.svelte
+++ extension/webviews/components/Initialize_join_component.svelte
@@ -1,17 +1,30 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+    const onSubmit = (data: string) => {
+        if (data == "Auth") {
+            dispatch("page_data_receive", { text: "Auth" });
+        } else {
+            dispatch("page_data_receive", { text: "invite" });
+        }
+    };
+</script>
+
 <p class="p_heading">Create new collaborative todo list<br /></p>
 <p class="p_content">
     Use this option if you want to start a new todo list to share with other
     people. A collaboration code will be generated
 </p>
-
-<button>Initialize todo list</button>
+<!-- Funny Svelt only allows passing param via inline only -->
+<button on:click={() => onSubmit("Auth")}>Initialize todo list</button>
 <p class="p_heading">Connect to existing Todo list<br /></p>
 <p class="p_content">
     Use this option if you want to join an existing todo list with an invite
     code
 </p>
-<button>Join a todo list</button>
+<button on:click={() => onSubmit("invite")}>Join a todo list</button>
 
+<!-- Funny Svelt only allows passing param via inline only -->
 <style>
     .p_heading {
         font-size: var(--vscode-font-size);
