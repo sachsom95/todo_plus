51cd7eb26aab448ad7c1be1cd2649e22d30f0929
Lucas Lobo <lucas5798fell@gmail.com>
2021-02-25 19:58:37 +0000

    Handle error on wrong code

--- extension/webviews/components/todoList.svelte
+++ extension/webviews/components/todoList.svelte
@@ -7,18 +7,29 @@
     import { TodoList } from "../../src/entities/TodoList";
     import { TodoItem } from "../../src/entities/TodoItem";
 
+    import { createEventDispatcher } from "svelte";
+
     export let code: string;
+
+    const dispatch = createEventDispatcher();
 
     let loading = true;
     let title = "";
     let makeTodoVisiblity = true;
     let description = "";
+    let notfound = false;
 
     let todoList: TodoList;
 
     (async () => {
         if (code !== undefined) {
-            todoList = await TodoList.fetch(code);
+            try {
+                todoList = await TodoList.fetch(code);
+            } catch (error) {
+                console.error(error);
+                notfound = true;
+                return;
+            }
         } else {
             todoList = await TodoList.create("My First TodoList!");
         }
@@ -42,114 +53,139 @@
         loading = false;
     }
 
+    function back() {
+        dispatch("page_data_receive", { page: "joinTodoListInvite" });
+    }
+
     // code for categories
     // let selectedCategory='Select Category'
     // let categories=['category1','category2','category3']
 </script>
 
-<h2 class="title">{todoList === undefined ? "Loading..." : todoList.name}</h2>
 
-<TodoCode code={todoList ? todoList.id : ""} />
+{#if notfound}
+    <h2 class="title">Error: TodoList not found!</h2>
+    <input class="form_input" disabled bind:value={code}>
+    <button class="btn" on:click={back}>Try another code</button>
+{:else}
+    <h2 class="title">{todoList === undefined ? "Loading..." : todoList.name}</h2>
 
-<div
-    class="maketodo-toggle"
-    on:click={() => {
-        makeTodoVisiblity = !makeTodoVisiblity;
-    }}
->
-    <p>Make Todos</p>
-    {#if makeTodoVisiblity}
-        <div class="icon"><i class="codicon codicon-chevron-up" /></div>
-    {:else}
-        <div class="icon"><i class="codicon codicon-chevron-down" /></div>
-    {/if}
-</div>
-{#if makeTodoVisiblity && todoList !== undefined}
-    <form
-        on:submit|preventDefault={() => {
-            if (title === "") return;
-            addTodo(title, description);
-            title = "";
-            description = "";
+    <TodoCode code={todoList ? todoList.id : ""} />
+
+    <div
+        class="maketodo-toggle"
+        on:click={() => {
+            makeTodoVisiblity = !makeTodoVisiblity;
         }}
     >
-        <p>Todo</p>
-        <input bind:value={title} />
-        <p>Description</p>
-        <textarea bind:value={description} />
+        <p>Make Todos</p>
+        {#if makeTodoVisiblity}
+            <div class="icon"><i class="codicon codicon-chevron-up" /></div>
+        {:else}
+            <div class="icon"><i class="codicon codicon-chevron-down" /></div>
+        {/if}
+    </div>
+    {#if makeTodoVisiblity && todoList !== undefined}
+        <form
+            on:submit|preventDefault={() => {
+                if (title === "") return;
+                addTodo(title, description);
+                title = "";
+                description = "";
+            }}
+        >
+            <p>Todo</p>
+            <input bind:value={title} />
+            <p>Description</p>
+            <textarea bind:value={description} />
 
-        <!-- Code for categories -->
-        <!-- <label>Category:</label>
-        <br/>
-        <select value={selectedCategory}>
-            {#each categories as category}
-                <option value={category}>
-                    {category}
-                </option>
-            {/each}
-        </select> -->
+            <!-- Code for categories -->
+            <!-- <label>Category:</label>
+            <br/>
+            <select value={selectedCategory}>
+                {#each categories as category}
+                    <option value={category}>
+                        {category}
+                    </option>
+                {/each}
+            </select> -->
 
+            <br />
+            <button class="btn" type="submit">Add Todo</button>
+        </form>
         <br />
-        <button class="btn" type="submit">Add Todo</button>
-    </form>
-    <br />
-{/if}
+    {/if}
 
-<hr />
+    <hr />
 
-<p>View Todos</p>
-<div class="todo_container">
-    <ul class="todolist">
-        <p class:hidden={!loading} class="transition">Loading...</p>
+    <p>View Todos</p>
+    <div class="todo_container">
+        <ul class="todolist">
+            <p class:hidden={!loading} class="transition">Loading...</p>
 
-        {#if todoList !== undefined}
-            {#each todoList.todoItems as todoItem (todoItem.id)}
-                <li
-                    class:completed={todoItem.completed}
-                    class="todoitem transition"
-                >
-                    <input
-                        class="todocheckbox"
-                        type="checkbox"
-                        checked={todoItem.completed}
-                        on:click={() => {
-                            todoItem.completed = !todoItem.completed;
-                        }}
-                    />
-                    <div class="todo-text">
-                        <h4>{todoItem.title}</h4>
-                        <p class="description">{todoItem.description}</p>
-                        <div class="interaction-buttons">
-                            <div class="icon-text">
-                                <div class="icon">
-                                    <i
-                                        class="codicon codicon-issues icon-align-fix"
-                                    />
+            {#if todoList !== undefined}
+                {#each todoList.todoItems as todoItem (todoItem.id)}
+                    <li
+                        class:completed={todoItem.completed}
+                        class="todoitem transition"
+                    >
+                        <input
+                            class="todocheckbox"
+                            type="checkbox"
+                            checked={todoItem.completed}
+                            on:click={() => {
+                                todoItem.completed = !todoItem.completed;
+                            }}
+                        />
+                        <div class="todo-text">
+                            <h4>{todoItem.title}</h4>
+                            <p class="description">{todoItem.description}</p>
+                            <div class="interaction-buttons">
+                                <div class="icon-text">
+                                    <div class="icon">
+                                        <i
+                                            class="codicon codicon-issues icon-align-fix"
+                                        />
+                                    </div>
+                                    <p>Push as an issue</p>
                                 </div>
-                                <p>Push as an issue</p>
-                            </div>
-                            <div
-                                class="icon-text"
-                                on:click={deleteTodo(todoItem)}
-                            >
-                                <div class="icon">
-                                    <i
-                                        class="codicon codicon-trash icon-align-fix"
-                                    />
+                                <div
+                                    class="icon-text"
+                                    on:click={deleteTodo(todoItem)}
+                                >
+                                    <div class="icon">
+                                        <i
+                                            class="codicon codicon-trash icon-align-fix"
+                                        />
+                                    </div>
+                                    <p>Delete</p>
                                 </div>
-                                <p>Delete</p>
                             </div>
                         </div>
-                    </div>
-                </li>
-            {:else}
-                <p>Add your first todo!</p>
-            {/each}
-        {/if}
-    </ul>
-</div>
+                    </li>
+                {:else}
+                    <p>Add your first todo!</p>
+                {/each}
+            {/if}
+        </ul>
+    </div>
+{/if}
+
+
 
 <style>
+
+    .form_input {   
+        padding-top: 10px;
+        padding-bottom: 10px;
+        text-align: center;
+        height: unset;
+    }
+
+    .form_input::placeholder {
+        text-align: center;
+    }
+
     .btn {
         padding-bottom: 10px;
         padding-top: 10px;
